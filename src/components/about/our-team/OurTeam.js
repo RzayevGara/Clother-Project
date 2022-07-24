8656d758319937f4378f37e3716323cbdaf298a5
RzayevGara <qara109@gmail.com>
2022-07-24 14:24:41 +0400

    about us team update

--- src/components/about/our-team/OurTeam.js
+++ src/components/about/our-team/OurTeam.js
@@ -1,37 +1,38 @@
-import FarhadImage from "../../../assets/images/picture/farhad.jpeg";
-import HuseynImage from "../../../assets/images/picture/huseyn.jpeg";
-import AyselImage from "../../../assets/images/picture/aysel.jpeg";
+import ElizabethImage from "../../../assets/images/picture/Elizabeth-image.png";
+import ScottImage from "../../../assets/images/picture/scott-image.png";
+import DanImage from "../../../assets/images/picture/Dan-image.png";
 
 function OurTeam() {
   return (
     <div className="our-team">
       <div className="our-team_container">
         <div>
-          <img src={AyselImage} alt="teamPicture" />
-          <h4>Aysel Sadigova</h4>
-          <p>Chief Marketing Officer</p>
-          {/* <p className="person-info">
+          <img src={ElizabethImage} alt="teamPicture" />
+          <h4>Elizabeth Spaulding</h4>
+          <p>Chief Executive Officer</p>
+          <p className="person-info">
             Elizabeth Spaulding is driving the next phase of the company`s
             growth as CEO
-          </p> */}
+          </p>
         </div>
         <div>
-          <img src={HuseynImage} alt="teamPicture" />
-          <h4>Huseyn Abizadeh</h4>
-          <p>Chief Executive  Officer</p>
-          {/* <p className="person-info">Scott is responsible for the company`s legal affairs.</p> */}
+          <img src={ScottImage} alt="teamPicture" />
+          <h4>Scott Darling</h4>
+          <p>Chief Legal Officer</p>
+          <p className="person-info">Scott is responsible for the company`s legal affairs.</p>
         </div>
         <div>
-          <img src={FarhadImage} alt="teamPicture" />
-          <h4>Farhad Aghayev</h4>
-          <p>Chief Technology Officer</p>
-          {/* <p className="person-info">
+          <img src={DanImage} alt="teamPicture" />
+          <h4>Dan Jedda</h4>
+          <p>Chief Financial Officer</p>
+          <p className="person-info">
             Dan Jedda leads the Finance team, and is deeply involved in
             expanding the growth opportunities ahead for the business.
-          </p> */}
+          </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
-export default OurTeam;
+
+export default OurTeam
